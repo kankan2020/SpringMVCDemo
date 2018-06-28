11379745be203ff7829a3352f855501d56ff0b63
kankan2020 <1102275035@qq.com>
2018-06-28 16:18:51 +0800

    test

---
+++ src/main/java/com/fencer/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.fencer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/app")
+public class LoginController{
+     @RequestMapping(value="/welcome")  //welcome要访问的url地址
+        public  String  hello(){
+            System.out.println("hello,springmvc");
+            return "hello";  //hello是逻辑视图名,和后缀名组合一起构成视图名  /web-inf/view/hello.jsp,
+        }
+}
